3c93c319dd3902c309101616f4cc35fafd9622c2
Lee Jaeyeon <1290jaeyeonlee@gmail.com>
2018-06-19 16:33:56 +0900

    Redirect 컴포넌트로 페이지 전환

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
   import React from 'react';
+  import {BrowserRouter, Route, Redirect} from 'react-router-dom';
   import TodoPage from './pages/TodoPage';
   import LoginPage from './pages/LoginPage';
-
-  import {PageProvider, PageConsumer} from './contexts/PageContext';
   import {UserProvider} from './contexts/UserContext';
-
 
   export default class App extends React.Component {
     render() {
       return (
-        <PageProvider>
+        <BrowserRouter>
           <UserProvider>
-            <PageConsumer>
-              {value => value.page === 'login' ? (
-                <LoginPage />
-              ) : value.page === 'todo' ? ( 
-                <TodoPage />
-              ) : null} 
-            </PageConsumer>
-          </UserProvider>  
-        </PageProvider>
-        // PageProvider 내부의 div는 
-        // PageContent.js 의 Provider의 props.children으로 들어간다. 
+            <Route path="/login" component={LoginPage}/>
+            <Route path="/todo" component={TodoPage}/>
+            <Route exact path="/" component={Home}/>
+          </UserProvider>
+        </BrowserRouter>  
       )
     }
   }
+  const Home = () => ( 
+    //함수형 컴포넌트
+    //const Home = (props) => ( // props생략
+    // 괄호 안에는 표현식을 써야한다.
+    localStorage.getItem('token') ? (
+      <Redirect to="/todo" />
+     ) : (
+      <Redirect to="/login" />
+     )
+  )
+  
--- src/containers/LogoutButtonContainer.js
+++ src/containers/LogoutButtonContainer.js
@@ -1,29 +1,27 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 import {UserConsumer} from '../contexts/UserContext';
-import {PageConsumer} from '../contexts/PageContext'
 
 export default class LogoutButtonContainer extends React.Component {
+  state = {
+    success: false
+  }
   render(){
-    return (
-      <UserConsumer>
-        {({logout}) =>(
-          <PageConsumer>
-          {({goToLoginPage}) => (
+    if(this.state.success) {
+      return (
+        <Redirect to="/login" />
+      )
+    } else {
+      return (
+        <UserConsumer>
+          {({logout}) =>(
             <button onClick={e => {
               logout();
-              goToLoginPage();
+              this.setState({success: true})
             }}>로그아웃</button>
           )}
-          </PageConsumer>
-        )}
-      </UserConsumer>
-          // <LoginForm onLogin={(username, password) => {
-          //   console.log(username, password)
-          // }
-          // />
-          // 인터페이스
-          // 로그인폼의 사용법은 onLogin에 username과 password를 넘겨주는 것이다.
-
-    )
+        </UserConsumer>
+      )
+    }
   }
 }
